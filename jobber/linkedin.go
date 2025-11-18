c0f5fb3e9d9a9a7b917953d25f85abbb50d133a4
Alvaroalonsobabbel <aalonso@babbel.com>
2025-11-18 11:18:34 +0100

    Add LinkedIn job scraping with tests and HTML parsing

---
+++ jobber/linkedin.go
@@ -0,0 +1,153 @@
+package jobber
+
+import (
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Alvaroalonsobabbel/jobber/db"
+	"github.com/PuerkitoBio/goquery"
+)
+
+const (
+	paramKeywords = "keywords" // Search keywords, ie. "golang"
+	paramLocation = "location" // Location of the search, ie. "Berlin"
+	paramStart    = "start"    // Start of the pagination, in intervals of 10s, ie. "10"
+
+	/*	Time Posted Range, ie.
+		- r86400` = Past 24 hours
+		- `r604800` = Past week
+		- `r2592000` = Past month
+		- `rALL` = Any time
+	*/
+	paramFTPR      = "f_TPR"
+	lastWeek       = "r604800" // Past week
+	searchInterval = 10        // LinkedIn pagination interval
+	linkedInURL    = "https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search"
+)
+
+type linkedIn struct {
+	client *http.Client
+	logger *slog.Logger
+}
+
+func NewLinkedIn(logger *slog.Logger) *linkedIn {
+	return &linkedIn{
+		client: &http.Client{Timeout: 10 * time.Second},
+		logger: logger,
+	}
+}
+
+func (l *linkedIn) PerformQuery(query *db.Query) ([]db.CreateOfferParams, error) {
+	var totalOffers []db.CreateOfferParams
+	var offers []db.CreateOfferParams
+
+	for i := 0; i == 0 || len(offers) == searchInterval; i += searchInterval {
+		resp, err := l.fetchOffersPage(query, i)
+		if err != nil {
+			return nil, fmt.Errorf("failed to fetchOffers in PerformQuery: %v", err)
+		}
+		offers, err = l.parseLinkedInBody(resp)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse LinkedIn body PerformQuery: %v", err)
+		}
+		totalOffers = append(totalOffers, offers...)
+	}
+
+	return totalOffers, nil
+}
+
+// fetchOffersPage gets job offers from LinkedIn based on the passed query params.
+// This returns a list of max 10 elements. We move the start by increments of 10.
+func (l *linkedIn) fetchOffersPage(query *db.Query, start int) (io.ReadCloser, error) {
+	qp := url.Values{}
+	qp.Add(paramKeywords, query.Keywords)
+
+	// TODO: make the FTPR param variable from when the last query was performed.
+	qp.Add(paramFTPR, lastWeek)
+	if query.Location != "" {
+		qp.Add(paramLocation, query.Location)
+	}
+	if start != 0 {
+		qp.Add(paramStart, strconv.Itoa(start))
+	}
+
+	url, err := url.Parse(linkedInURL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse URL: %w", err)
+	}
+	url.RawQuery = qp.Encode()
+
+	// TODO: implement retry mechanism.
+	resp, err := l.client.Get(url.String())
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch URL: %w", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("received status code: %d", resp.StatusCode)
+	}
+	return resp.Body, nil
+}
+
+// Parse parses the LinkedIn HTML response and returns a list of jobs.
+func (l *linkedIn) parseLinkedInBody(body io.ReadCloser) ([]db.CreateOfferParams, error) {
+	doc, err := goquery.NewDocumentFromReader(body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse HTML: %w", err)
+	}
+	body.Close()
+	var jobs []db.CreateOfferParams
+
+	// Find all job listings
+	doc.Find("li").Each(func(_ int, s *goquery.Selection) {
+		// Check if this li contains a job card
+		if s.Find(".base-search-card").Length() > 0 {
+			job := db.CreateOfferParams{}
+
+			// Extract Job ID from data-entity-urn
+			if urn, exists := s.Find("[data-entity-urn]").Attr("data-entity-urn"); exists {
+				id := strings.Split(urn, ":")
+				job.ID = id[len(id)-1]
+			}
+
+			// Extract Title
+			job.Title = normalize(s.Find(".base-search-card__title").Text())
+
+			// Extract Company
+			job.Company = normalize(s.Find(".base-search-card__subtitle a").Text())
+
+			// Extract Location
+			job.Location = normalize(s.Find(".job-search-card__location").Text())
+
+			// Extract Posted Date
+			postedAt, _ := s.Find("time").Attr("datetime")
+			t, err := time.Parse("2006-01-02", postedAt)
+			if err != nil {
+				l.logger.Error("unable to parse datetime for job ID ", job.ID, slog.String("error", err.Error()))
+			}
+			job.PostedAt = t
+
+			// Only add if we have essential data
+			if job.ID != "" && job.Title != "" {
+				jobs = append(jobs, job)
+			} else {
+				l.logger.Error("Missing essential data for job ID", slog.String("jobID", job.ID))
+			}
+		}
+	})
+
+	return jobs, nil
+}
+
+func normalize(s string) string {
+	str := strings.Split(s, "\n")
+	for i, v := range str {
+		str[i] = strings.TrimSpace(v)
+	}
+	return strings.TrimSpace(strings.Join(str, " "))
+}
---
+++ jobber/linkedin_test.go
@@ -0,0 +1,143 @@
+package jobber
+
+import (
+	"io"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/Alvaroalonsobabbel/jobber/db"
+)
+
+func TestFetchOffersPage(t *testing.T) {
+	mockResp := newLinkedInMockResp(t)
+	l := &linkedIn{
+		client: &http.Client{Transport: mockResp},
+		logger: slog.New(slog.NewTextHandler(io.Discard, &slog.HandlerOptions{})),
+	}
+	query := &db.Query{
+		Keywords: "golang",
+		Location: "the moon",
+	}
+	resp, err := l.fetchOffersPage(query, 0)
+	if err != nil {
+		t.Errorf("error fetching offers: %s", err.Error())
+	}
+	defer resp.Close()
+	values := mockResp.req.URL.Query()
+	if values.Get(paramKeywords) != "golang" {
+		t.Errorf("expected 'keywords' in query params to be 'golang', got %s", values.Get(paramKeywords))
+	}
+	if values.Get(paramLocation) != "the moon" {
+		t.Errorf("expected 'location' in query params to be 'the moon', got %s", values.Get(paramLocation))
+	}
+	if values.Get(paramFTPR) != lastWeek {
+		t.Errorf("expected 'f_TPR' in query params to be lastlastWeek, got %s", values.Get(paramFTPR))
+	}
+	if mockResp.req.URL.Host != "www.linkedin.com" {
+		t.Errorf("expected host to be 'www.linkedin.com', got %s", mockResp.req.URL.Host)
+	}
+	if mockResp.req.URL.Path != "/jobs-guest/jobs/api/seeMoreJobPostings/search" {
+		t.Errorf("expected path to be '/jobs-guest/jobs/api/seeMoreJobPostings/search', got %s", mockResp.req.URL.Path)
+	}
+	file, err := os.Open("example1.html")
+	if err != nil {
+		t.Fatalf("failed to open file: %s", err.Error())
+	}
+	defer file.Close()
+	want, err := io.ReadAll(file)
+	if err != nil {
+		t.Fatalf("failed to read example1.html file: %s", err.Error())
+	}
+	got, err := io.ReadAll(resp)
+	if err != nil {
+		t.Errorf("unable to read response body: %v", err)
+	}
+	if len(want) != len(got) {
+		t.Errorf("expected response body length to be %d, got %d", len(want), len(got))
+	}
+}
+
+func TestParseLinkedInBody(t *testing.T) {
+	l := &linkedIn{logger: slog.New(slog.NewTextHandler(io.Discard, &slog.HandlerOptions{}))}
+
+	file, err := os.Open("example1.html")
+	if err != nil {
+		log.Fatalf("failed to open file: %s", err.Error())
+	}
+	defer file.Close()
+
+	jobs, err := l.parseLinkedInBody(file)
+	if err != nil {
+		t.Fatalf("error parsing example.html: %s", err.Error())
+	}
+	if len(jobs) != 10 {
+		t.Errorf("expected 10 jobs, got %d", len(jobs))
+	}
+	if jobs[0].ID != "4322119156" {
+		t.Errorf("expected job ID 4322119156, got %s", jobs[0].ID)
+	}
+	if jobs[0].Title != "Software Engineer (Golang)" {
+		t.Errorf("expected job title 'Software Engineer (Golang)', got '%s'", jobs[0].Title)
+	}
+	if jobs[0].Location != "Berlin, Berlin, Germany" {
+		t.Errorf("expected job location 'Berlin, Berlin, Germany', got '%s'", jobs[0].Location)
+	}
+	if jobs[0].Company != "Delivery Hero" {
+		t.Errorf("expected job company 'Delivery Hero', got '%s'", jobs[0].Company)
+	}
+	if jobs[0].PostedAt.Format("2006-01-02") != "2025-11-13" {
+		t.Errorf("expected job posted at time %v, got %v", "2025-11-13", jobs[0].PostedAt.Format("2006-01-02"))
+	}
+}
+
+func TestPerformQuery(t *testing.T) {
+	mockResp := newLinkedInMockResp(t)
+	l := &linkedIn{
+		client: &http.Client{Transport: mockResp},
+		logger: slog.New(slog.NewTextHandler(io.Discard, &slog.HandlerOptions{})),
+	}
+	query := &db.Query{
+		Keywords: "golang",
+		Location: "the moon",
+	}
+	offers, err := l.PerformQuery(query)
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+	if len(offers) != 27 {
+		t.Errorf("expected 27 offers, got %d", len(offers))
+	}
+}
+
+type linkedInMockResp struct {
+	t   testing.TB
+	req *http.Request
+}
+
+func (h *linkedInMockResp) RoundTrip(req *http.Request) (*http.Response, error) {
+	h.req = req
+	fn := "example1.html"
+	switch h.req.URL.Query().Get("start") {
+	case "10":
+		fn = "example2.html"
+	case "20":
+		fn = "example3.html"
+	}
+
+	body, err := os.Open(fn)
+	if err != nil {
+		h.t.Fatalf("failed to open %s in mockResp.RoundTrip: %s", fn, err)
+	}
+
+	return &http.Response{
+		StatusCode: http.StatusOK,
+		Body:       body,
+	}, nil
+}
+
+func newLinkedInMockResp(t testing.TB) *linkedInMockResp {
+	return &linkedInMockResp{t: t}
+}
